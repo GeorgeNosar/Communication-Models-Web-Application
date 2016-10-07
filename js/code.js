9d887c6045244cfc3eebd20a81c4db867c28bd85
lamill <causedeer@gmail.com>
2016-10-07 12:10:58 +0300

    check for type of input data

--- js/code.js
+++ js/code.js
@@ -19,24 +19,33 @@ function CountForMM1() {
     		var form = document.forms.input;
     		var lyambda = form.elements.lyambda.value;
     		var myu = form.elements.myu.value;
-    		alert(lyambda);
-    		alert(myu);
-    		var max = 11;
-    		var pk = [];
-    		if (myu > lyambda) 
-    		{
-    			for(var i = 0; i < max; i++) 
-    			{
-    				pk[i] = CountPkMM1(lyambda, myu, i);
-    			}
-    			var k = CountKMM1(lyambda, myu);
-    			var t = CountTMM1(lyambda, myu);
-    			alert(pk);
-    			alert(k);
-    			alert(t);
-    		}
-    		else
-    		{
-    			alert("Invalid values. Try M > L");
-    		}
+            if((typeof lyambda=="number")&&(typeof myu=="number"))
+            {
+                alert(lyambda);
+                 alert(myu);
+                var max = 11;
+                var pk = [];
+                if (myu > lyambda) 
+                {
+                for(var i = 0; i < max; i++) 
+                {
+                    pk[i] = CountPkMM1(lyambda, myu, i);
+                }
+                var k = CountKMM1(lyambda, myu);
+                var t = CountTMM1(lyambda, myu);
+                alert(pk);
+                alert(k);
+                alert(t);
+                }
+                else
+                {
+                   alert("Invalid values. Try M > L");
+                }
+            }
+    		else 
+            {
+                alert("Not a number");
+            }
     	};
+
+//function CountForMMV
